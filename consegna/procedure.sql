12a009f96098ce89a9b686eba5358a52b6cd0378
Giuseppe Iavarone <54623419+javar0ne@users.noreply.github.com>
2024-07-14 22:03:22 +0200

    Update procedure.sql

--- consegna/procedure.sql
+++ consegna/procedure.sql
@@ -282,3 +282,27 @@ BEGIN
 		DBMS_OUTPUT.PUT_LINE('Si hanno a magazzino ' || qta_da_produrre || ' pezzi del prodotto finito ' || p_CodiceProdottoFinito);
 	END IF;
 END;
+
+/* Procedura che dato il cf di un dipendente, ritorna se quest'ultimo può essere impegnato nel turno identificato dalle date di inizio e fine turno */
+CREATE OR REPLACE PROCEDURE "C##DB_COMET".VERIFICA_TURNI(
+	p_CFDipendente		IN DIPENDENTE.CODICEFISCALE%TYPE,
+	p_DataInizioTurno	IN DATE,
+	p_DataFineTurno		IN DATE
+) IS
+	count_turni NUMBER;
+BEGIN
+	SELECT COUNT(*)
+	INTO count_turni 
+	FROM TURNO t 
+	WHERE 
+	(
+		(p_DataInizioTurno BETWEEN t.DATAINIZIOTURNO AND t.DATAFINETURNO) OR 
+		(p_DataFineTurno BETWEEN t.DATAINIZIOTURNO AND t.DATAFINETURNO)
+	) AND t.CFDIPENDENTE = p_CFDipendente;
+
+	IF count_turni > 0 THEN
+		DBMS_OUTPUT.PUT_LINE('Il dipendente con codice fiscale ' || p_CFDipendente || ' è già impegnato nel periodo indicato');
+	ELSE 
+		DBMS_OUTPUT.PUT_LINE('Per il dipendente con codice fiscale ' || p_CFDipendente || ' può essere inserito un turno nel periodo indicato.');
+	END IF;
+END;
